a300260e39a0ca28034611686369e5130939fc39
Galvotas <a.andrulis@yahoo.com>
2020-09-30 19:19:33 +0200

    responsive changes

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 const menuModul = document.querySelector('.menu-abs')
 const burgerBtn = document.querySelector('.hamburger')
-
-
+const removeBtn = document.querySelector('.fa-times')
+const reservationModul = document.querySelector('.contact-form-wrapper')
+const reservationBtn = document.querySelector('.reservation')
+const header = document.querySelector('.header')
 window.addEventListener('click', (e) => {
 if(e.target.classList.contains('hamburger') || e.target.classList.contains('line')) {
     menuModul.classList.toggle('active');
 }
+})
+reservationModul.classList.remove('close-animation')
+
+window.addEventListener('click', (e) => {
+if(e.target === reservationBtn) {
+    reservationModul.classList.add('form-active')
+    reservationModul.classList.remove('close-animation')
+
+} 
+if(e.target === removeBtn) {
+    reservationModul.classList.remove('form-active')
+    reservationModul.classList.add('close-animation')
+
+}
+
+})
+
+
+window.addEventListener('scroll', () => {
+    const navBar = document.querySelector('.navigation')
+const height = navBar.clientHeight
+    if(window.scrollY >= 40) {
+        header.style.paddingTop = height + 'px';
+        navBar.classList.add('fixed')
+    } else {
+        header.style.paddingTop = 0;
+
+    navBar.classList.remove('fixed')
+
+    }
+    if(menuModul.classList.contains('active')) {
+        menuModul.classList.remove('active')
+    }
 })
